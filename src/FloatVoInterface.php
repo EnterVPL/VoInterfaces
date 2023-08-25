3835c52108551a5908119a372e2702ba37487897
Łukasz <l.bieniek@clawrock.com>
2023-08-25 11:09:12 +0200

    init

---
+++ src/FloatVoInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EnterV\VoInterfaces;
+
+declare(strict_types=1);
+
+interface FloatVoInterface
+{
+    public function value(): float;
+}
---
+++ src/FormattedDateTimeVoInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace EnterV\VoInterfaces;
+
+declare(strict_types=1);
+
+interface FormattedDateTimeVoInterface
+{
+    public function formatted(): string;
+}
